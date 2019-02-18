1b4e2fdda51241c73db5447dc23d1ef789e2f94d
jorgecarloscontr <jorge_carloscontr@hotmail.com>
2019-02-18 16:50:07 -0600

    creacion de archivo.readme

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/info',function(){
+    return view('paginas.informacion');
+    //return "Hola informacion del sistema";
+});
+Route::get('/contacto',function(){
+    return view('paginas.contacto');
+});
